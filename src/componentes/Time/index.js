d4817ada33262255243c52926e0ad184c2c6668c
Andressa <andressa.machado256@gmail.com>
2022-12-20 17:42:39 -0300

    Retomando o projeto

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -10,15 +10,17 @@ const Time = (props) => {
       >
         <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
         <div className="colaboradores">
-          {props.colaboradores.map((colaborador) => (
-            <Colaborador
-              key={colaborador.nome}
-              corDeFundo={props.corPrimaria}
-              nome={colaborador.nome}
-              cargo={colaborador.cargo}
-              imagem={colaborador.imagem}
-            />
-          ))}
+          {props.colaboradores.map((colaborador) => {
+            return (
+              <Colaborador
+                key={colaborador.nome}
+                corDeFundo={props.corPrimaria}
+                nome={colaborador.nome}
+                cargo={colaborador.cargo}
+                imagem={colaborador.imagem}
+              />
+            );
+          })}
         </div>
       </section>
     )
