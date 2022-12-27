8dc0c6faf695b2919fb420175bf43864cefdda05
Andressa <andressa.machado256@gmail.com>
2022-12-27 19:00:10 -0300

    Criando novos times e removendo colaboradores

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = (props) => {
+  const id = uuidv4();
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
   const [imagem, setImagem] = useState("");
   const [time, setTime] = useState("");
+  const [nomeTime, setNomeTime] = useState("");
+  const [corTime, setCorTime] = useState("");
 
   const aoSalvar = (event) => {
     event.preventDefault();
     props.aoColaboradorCadastrado({
+      id,
       nome,
       cargo,
       imagem,
@@ -56,6 +61,29 @@ const Formulario = (props) => {
           aoAlterado={(valor) => setTime(valor)}
         />
         <Botao>Criar card</Botao>
+      </form>
+      <form
+        onSubmit={(evento) => {
+          evento.preventDefault();
+          props.cadastrarTime({ nome: nomeTime, cor: corTime });
+        }}
+      >
+        <h2>Preencha os dados para criar um novo time</h2>
+        <CampoTexto
+          obrigatorio
+          label="Nome do time"
+          placeholder="Digite o nome do time"
+          valor={nomeTime}
+          aoAlterado={(valor) => setNomeTime(valor)}
+        />
+        <CampoTexto
+          obrigatorio
+          label="Cor do time"
+          placeholder="Digite a cor do time"
+          valor={corTime}
+          aoAlterado={(valor) => setCorTime(valor)}
+        />
+        <Botao>Criar um novo time</Botao>
       </form>
     </section>
   );
